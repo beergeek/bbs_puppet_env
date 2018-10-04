b5f21b1daa8fcd388e9bf96da0d9127a404bb0a7
beergeek <brett@puppet.com>
2018-10-04 16:40:23 +0900

    Updates to Tomcat

--- site/profile/manifests/tomcat_services.pp
+++ site/profile/manifests/tomcat_services.pp
@@ -1,12 +1,34 @@
-class profile::tomcat_services (
-  $tomcat_servers,
+class profile::web_services::tomcat_services (
+  Hash $tomcat_installs,
+  Hash $tomcat_servers,
+  Hash $tomcat_instances,
+  Hash $tomcat_connectors,
 ) {
 
-  class { 'tomcat': }
+  class { 'java': }
 
-  $tom_instances.each |String $server_name, Hash $server_info| {
-    tomcat::config::instance { $server_name:
-      * => $server_info,;
+  $tomcat_installs.each |String $install_name, Hash $install_info| {
+    tomcat::install { $install_name:
+      * => $install_info,
+    }
+  }
+
+  $tomcat_instances.each |String $instance_name, Hash $instance_info| {
+    tomcat::config::instance { $instance_name:
+      * => $instance_info,;
+    }
+  }
+
+  $tomcat_servers.each |String $server_name, Hash $server_info| {
+    tomcat::config::server { $server_name:
+      * => $server_info,
+    }
+  }
+
+  $tomcat_connectors.each |String $connector_name, Hash $connector_info| {
+    tomcat::config::server::connector { $connector_name:
+      * => $connector_info,
     }
   }
 }
+
